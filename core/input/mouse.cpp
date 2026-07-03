c824122b2adaa94a608199cf6c7de4f11c278fa2
Bradly Landucci <salvesoundsofficial@gmail.com>
2026-07-03 16:39:53 -0700

    #5 Added simple mouse handling

---
+++ core/input/mouse.cpp
@@ -0,0 +1,42 @@
+#include <input/mouse.h>
+
+#include <magic_enum/magic_enum.hpp>
+
+//------------------------------------------------------------------//
+
+void Mouse::initialize(GLFWwindow *window)
+{
+    glfwSetCursorPosCallback(window, Mouse::processCursorMove);
+    glfwSetCursorEnterCallback(window, Mouse::processCursorEnter);
+    glfwSetMouseButtonCallback(window, Mouse::processCursorClick);
+}
+
+//------------------------------------------------------------------//
+
+bool Mouse::isPressed(ButtonType button)
+{
+    return m_pressedButton == button;
+}
+
+//------------------------------------------------------------------//
+
+void Mouse::processCursorMove(GLFWwindow *window, double xPos, double yPos)
+{
+    m_cursorPosition = Vector2{ xPos, yPos };
+}
+
+//------------------------------------------------------------------//
+
+void Mouse::processCursorEnter(GLFWwindow *window, int entered)
+{
+    m_cursorInWindow = (entered == GLFW_TRUE) ? true : false;
+}
+
+//------------------------------------------------------------------//
+
+void Mouse::processCursorClick(GLFWwindow *window, int button, int action, int mods)
+{
+    // Send event
+}
+
+//------------------------------------------------------------------//
