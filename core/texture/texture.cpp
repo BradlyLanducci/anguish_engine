b8362a68b0827daa3ee437eb3e3dffd6b9382a9b
Bradly Landucci <salvesoundsofficial@gmail.com>
2026-07-10 23:08:33 -0700

    Render all textures with nearest neighbor filtering

--- core/texture/texture.cpp
+++ core/texture/texture.cpp
@@ -22,9 +22,9 @@ Texture::Texture()
     checkGLError();
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     checkGLError();
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     checkGLError();
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     checkGLError();
 }
 
